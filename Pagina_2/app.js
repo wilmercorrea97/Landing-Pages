c042ce3afe0ead7447e63bc4c0bb6bd1c096adef
wilmercorrea97 <wilmercorrea97@gmail.com>
2022-02-23 16:32:15 -0300

    Paginas Web

---
+++ Pagina_2/app.js
@@ -0,0 +1,7 @@
+function toogleMenu(){
+    let menuToggle = document.querySelector('.menuToggle');
+    let navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
